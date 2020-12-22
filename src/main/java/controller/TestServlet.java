a37d43377f092d38a88d19300f68f86b3dfafda0
Yan Tasheiko <yantasheiko@gmail.com>
2020-12-22 16:44:21 +0300

    created simple app for testing CiCd on jenkins/centos

---
+++ src/main/java/controller/TestServlet.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset = utf-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        String str = "Hello on CentOs from Jenkins";
+
+        req.setAttribute("hello", str);
+        getServletContext().getRequestDispatcher("/jsp/hello.jsp").forward(req, resp);
+    }
+}
